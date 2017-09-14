aefc4e627f0e27d31179cd16f9f5032af224ca63
Elisa Vaccaro <erv230@nyu.edu>
2017-09-14 01:05:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ function Objects(){
 var recipes = new Objects();
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newKey = key;
-  return Object.assign(object, {[key]: value});
+  return Object.assign({}, object, {[key]: value});
 }
